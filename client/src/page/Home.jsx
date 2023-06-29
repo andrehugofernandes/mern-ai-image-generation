a387eaeee1878de317e600a664e5a77dd32de03c
andrehugofernandes <andrehugofernandes@gmail.com>
2023-06-29 16:55:05 -0300

    changing url server

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,8 +26,8 @@ const Home = () => {
     setLoading(true);
 
     try {
-      //const response = await fetch('https://dall-e2-4ax7.onrender.com/api/v1/post', {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://mern-ai-image.onrender.com/api/v1/post', {
+      //const response = await fetch('http://localhost:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
